64736c7da65dd5e752cfc61f73c25915f82a6bf3
GasFeDev <gasfedev@gmail.com>
2022-08-24 16:03:23 -0300

    8 commit

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -23,13 +22,13 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN
-/* router.post("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     !user && res.status(404).json("Wrong password or username!");
 
     const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
-    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+    const originalPassword = await bytes.toString(CryptoJS.enc.Utf8);
 
     originalPassword !== req.body.password &&
       res.status(401).json("Wrong password or username!");
@@ -43,23 +42,6 @@ router.post("/register", async (req, res) => {
     const { password, ...info } = user._doc;
 
     res.status(200).json({ ...info, accessToken });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-}); */
-
-router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).json("user not found");
-
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    !validPassword && res.status(400).json("wrong password");
-
-    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
