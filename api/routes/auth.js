9e772c5f6a82896eebb672e9876d046ec4148ea3
GasFeDev <gasfedev@gmail.com>
2022-08-24 18:48:02 -0300

    8 commit

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -30,12 +31,11 @@ router.post("/login", async (req, res) => {
       return res.status(404).json("Wrong password or username!");
     }
 
-    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
-    const originalPassword = await bytes.toString(CryptoJS.enc.Utf8);
+    const compPass = await bcrypt.compare(req.body.password, user.password);
 
-    if (originalPassword !== req.body.password) {
+    if (!compPass) {
       return;
-      res.status(401).json("Wrong password or username!");
+      res.status(400).json("Wrong password or username!");
     }
 
     const accessToken = jwt.sign(
