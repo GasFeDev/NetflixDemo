c6985e26d4c6e4ddc06c59c3e3cccf19c746b7a3
GasFeDev <gasfedev@gmail.com>
2022-08-24 23:04:06 -0300

    8 commit

--- api/client/src/authContext/apiCalls.js
+++ api/client/src/authContext/apiCalls.js
@@ -1,10 +1,10 @@
-import { axiosInstance } from "../config";
+import { axios } from "axios";
 import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axiosInstance.post("auth/login", user);
+    const res = await axios.post("auth/login", user);
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
